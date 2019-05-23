2133c3c51fdfba614fb0aa9a0a8944957944ef1c
Ryan Lima <ryanlima08021997@gmail.com>
2019-05-23 02:40:22 -0300

    Mascaras com Vanilla JS

---
+++ js/tutors/Mascaras/app.js
@@ -0,0 +1,33 @@
+const masks = {
+    cpf(value){
+        return value
+        .replace(/\D/g, '')
+        .replace(/(\d{3})(\d)/, '$1.$2')
+        .replace(/(\d{3})(\d)/, '$1.$2')
+        .replace(/(\d{3})(\d{1,2})/, '$1-$2')
+        .replace(/(-\d{2})\d+?$/, '$1')
+    },
+
+    cnpj(value){
+        return value
+        .replace(/\D/g, "")
+        .replace(/(\d{2})(\d)/, '$1.$2')
+        .replace(/(\d{3})(\d)/, '$1.$2')
+        .replace(/(\d{3})(\d)/, '$1/$2')
+        .replace(/(\d{4})(\d{1,2})/, '$1-$2')
+        .replace(/(-\d{2})\d+?$/, '$1')
+        
+    }
+}
+
+
+// https://www.youtube.com/watch?v=r-8isv_TnVA&feature=youtu.be
+//19:42
+
+document.querySelectorAll('input').forEach(($input) => {
+    const field = $input.dataset.js
+
+    $input.addEventListener('input', (e) => {
+        e.target.value = masks[field](e.target.value)
+    }, false)
+});
