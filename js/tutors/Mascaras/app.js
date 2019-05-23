1a569962dd0485e3abfb6d61404624b742e40f4e
Ryan Lima <ryanlima08021997@gmail.com>
2019-05-23 17:42:26 -0300

    finalizando mascaras com js

--- js/tutors/Mascaras/app.js
+++ js/tutors/Mascaras/app.js
@@ -16,8 +16,33 @@ const masks = {
         .replace(/(\d{3})(\d)/, '$1/$2')
         .replace(/(\d{4})(\d{1,2})/, '$1-$2')
         .replace(/(-\d{2})\d+?$/, '$1')
-        
+    },
+
+    phone(value){
+        return value
+        .replace(/\D/g, '')
+        .replace(/(\d{2})(\d)/, '($1) $2')
+        .replace(/(\d{4})(\d)/, '$1-$2')
+        .replace(/(\d{4})-(\d)(\d{4})/, '$1$2-$3')
+        .replace(/(-\d{4})\d+?$/, '$1')
+    },
+
+    cep(value){
+        return value
+        .replace(/\D/g, '')
+        .replace(/(\d{5})(\d)/, '$1-$2')
+        .replace(/(-\d{3})\d+?$/, '$1')
+    },
+
+    pis(value){
+        return value
+        .replace(/\D/g, '')
+        .replace(/(\d{3})(\d)/, '$1.$2')
+        .replace(/(\d{5})(\d)/, '$1.$2')
+        .replace(/(\d{5}\.)(\d{2})(\d)/, '$1$2-$3')
+        .replace(/(-\d)\d+?$/, '$1')
     }
+    
 }
 
 
