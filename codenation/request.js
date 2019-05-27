8343efc22710aabb0b0433230500f5e506a2f40b
ryanlima <37602101+ryanlima@users.noreply.github.com>
2019-05-27 08:55:03 -0300

    Update request.js

--- codenation/request.js
+++ codenation/request.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 
 exports.requestJson = function () {
   
-    request('https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=9ce207caed5bb19435d7ddc3c188c357abe24fc7', function(error, response, body){
+    request('https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=TOKEN', function(error, response, body){
         console.log('error:', error);
         console.log('statusCode:', response && response.statusCode);
         console.log('body:', body);
