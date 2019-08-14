dc2cb40471e9d18bb5588e843a3817d69b89e251
Ryan Lima <ryanlima08021997@gmail.com>
2019-08-14 17:45:20 -0300

    Listando despesas - app es6

--- js/es6/app1/app.js
+++ js/es6/app1/app.js
@@ -16,6 +16,7 @@ class Despesa{
         }
         return true
     }
+
 }
 
 class BD {
@@ -40,6 +41,28 @@ class BD {
         localStorage.setItem(id, JSON.stringify(d))
 
         localStorage.setItem('id', id)
+    }
+
+    recuperarTodosRegistros(){
+
+        // array de despesas 
+        let despesas = Array()
+
+        let id = localStorage.getItem('id')
+        //recupera todas as despesas cadastradas em localStorage
+        for(let i = 1; i <= id; i++){
+            //recupera a despesa
+            let despesa = JSON.parse(localStorage.getItem(i))
+
+            //existe a possibilidade de haver indices que foram removidos
+            // nestes casos nós vamos pular esses indices
+            if(despesas === null){
+                continue
+            }
+            despesas.push(despesa)
+        }
+
+        return despesas
     }
 }
 
@@ -99,4 +122,45 @@ function exibeModal(tipo, titulo, descricao, descricao_button ){
         return false
     }
     $('#modalRegistraDepesa').modal('show')
+}
+
+function carregaListaDespesas(){
+    let despesas = Array()
+
+    despesas = bd.recuperarTodosRegistros()
+
+    //selecionado elemento tbody
+    var listaDespesas = document.getElementById('listaDespesas')
+    console.log(listaDespesas)
+
+    // <tr>
+    //     <td>15/03/2018</td>
+    //     <td>Alimentação</td>
+    //     <td>Compras</td>
+    //     <td>300</td>
+    // </tr> 
+    //pecorrer o array despesas, luistando cada despesa de forma dinâmica
+    despesas.forEach(function(d){
+        //criando o tr
+        let linha = listaDespesas.insertRow()
+console.log(d)
+        //ajustar tipo
+        switch(d.tipo){
+            case '1': d.tipo = 'Alimentação'
+                break
+            case '2': d.tipo = 'Educação'
+                break
+            case '3': d.tipo = 'Lazer'
+                break
+            case '4': d.tipo = 'Saúde'
+                break
+            case '5': d.tipo = 'Transporte'
+                break
+        }
+        // criar as colunas
+        linha.insertCell(0).innerHTML = `${d.dia}/${d.mes}/${d.ano}` 
+        linha.insertCell(1).innerHTML = d.tipo
+        linha.insertCell(2).innerHTML = d.descricao
+        linha.insertCell(3).innerHTML = d.valor
+    })
 }
